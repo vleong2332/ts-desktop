446d27affac6e7580f2f2e24cc0aa243892872eb
VICKY LEONG <VLEONG2332@GMAIL.COM>
2016-06-27 23:04:58 -0400

    Create language request object when clicking submit

--- src/js/questionnaire.js
+++ src/js/questionnaire.js
@@ -3,7 +3,11 @@
 'use strict';
 
 
-let _ = require('lodash');
+let _ = require('lodash'),
+    sanitizer = require('sanitizer'),
+    uuid = require('node-uuid'),
+    temp = require('./temporary'),
+    utils = require('./lib/utils');
 
 
 function Questionnaire(languageSlug) {
@@ -29,9 +33,47 @@ function Questionnaire(languageSlug) {
             return questionnaire && groupQuestions(questionnaire.questions);
         },
 
+        // Return the loaded questionnaire id
+        get id() {
+            console.log('in id', questionnaire);
+            return questionnaire && questionnaire['questionnaire_id'];
+        },
+
+        // Return the data_fields/language_data object from the questionnaire
+        get dataFields() {
+            console.log('in dataFields', questionnaire);
+            // return questionnaire && questionnaire['data-fields'];
+            return questionnaire && questionnaire['language_data'];
+        },
+
+        // Clean answers. Checkout https://github.com/theSmaw/Caja-HTML-Sanitizer/blob/master/test/test-sanitizer.js
+        // for more info on what it does.
+        sanitize: function(answersObj) {
+            _.forIn(answersObj, function(value, key) {
+                value = sanitizer.sanitize(value);
+            });
+            return answersObj;
+        },
+
         // Save user's answer and some other metadata as a file to be pushed later
-        save: function() {
-            throw 'Not implemented';
+        save: function(answersObj) {
+            let _this = this;
+            temp.newCode()
+                .then(function(tempCode) {
+                    return {
+                        request_id: uuid.v1(),
+                        temp_code: tempCode,
+                        data_fields: _this.dataFields,
+                        questionnaire_id: _this.id,
+                        app: 'ts-desktop',
+                        requester: '',
+                        submitted_at: Date.now(),
+                        answers: _.map(answersObj, function(answer, key) {
+                            return {question_id: key, text: answer};
+                        })
+                    };
+                })
+                .then(utils.log);
         },
 
         // Submit questionnaire/answers file to door43 and tD
